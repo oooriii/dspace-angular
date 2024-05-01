e17fdb19c56543c89e7a6a4354c451ab32047ecd
oooriii <oooriii@gmail.com>
2024-05-01 12:51:45 +0200

    pre merge

--- src/themes/udg/eager-theme.module.ts
+++ src/themes/udg/eager-theme.module.ts
@@ -57,6 +57,7 @@ import { TopLevelCommunityListComponent } from './app/home-page/top-level-commun
 import { LogInComponent } from './app/shared/log-in/log-in.component';
 import { BrowseEntryListElementComponent } from './app/shared/object-list/browse-entry-list-element/browse-entry-list-element.component';
 import { PersonComponent } from './app/entity-groups/research-entities/item-pages/person/person.component';
+// cal?
 //import { LangSwitchComponent } from './app/shared/lang-switch/lang-switch.component';
 
 // oriol - 20240426 - intentant crear nou component

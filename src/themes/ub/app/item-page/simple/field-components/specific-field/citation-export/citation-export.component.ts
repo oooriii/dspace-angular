33fe0212cccc31e4e4bec9a927e4de1ab683e7a7
Oriol <oooriii@gmail.com>
2025-07-28 11:47:33 +0200

    citation component iso 690

--- src/themes/ub/app/item-page/simple/field-components/specific-field/citation-export/citation-export.component.ts
+++ src/themes/ub/app/item-page/simple/field-components/specific-field/citation-export/citation-export.component.ts
@@ -23,8 +23,12 @@ export class CitationExportComponent implements OnInit {
 
   constructor(private clipboard: Clipboard) {}
 
-  selectedFormat = 'apa';
+  //electedFormat = 'apa';
+  selectedFormat = 'iso690';
   isExpanded = false;
+
+  locale = 'ca'; // TODO: get the locale from the user
+
 
   citationFormats: CitationFormat[] = [
     { value: 'iso690', label: 'ISO 690' },
@@ -37,9 +41,31 @@ export class CitationExportComponent implements OnInit {
     { value: 'ris', label: 'RIS' }
   ];
 
+  /*
   ngOnInit(): void {
     // Component initialization
   }
+  */
+   /**
+   * Get the user's locale from the browser.
+   * 
+   * The browser provides a property called `navigator.language` (or `navigator.languages`)
+   * which tells us the user's preferred language/locale setting.
+   * 
+   * Example values: 'en-US', 'ca', 'es-ES'
+   */
+   getUserLocale(): string {
+    // navigator.language returns the browser's primary language setting
+    // If not available, default to 'ca_ES'
+    return navigator.language || 'ca_ES';
+  }
+
+  ngOnInit(): void {
+    // Set the locale property to the user's browser locale on component initialization
+    this.locale = this.getUserLocale();
+    // You can add other initialization code here if needed
+  }
+
 
   /**
    * Toggle the expanded state of the citation export
@@ -89,6 +115,8 @@ export class CitationExportComponent implements OnInit {
   }
   /**
    * Generate the citation in the selected format
+   * @param format
+   * @returns text
    */
   private generateCitation(format: string): string {
     switch (format) {
@@ -113,11 +141,131 @@ export class CitationExportComponent implements OnInit {
     }
   }
 
+  /**
+   * Generate the ISO 690 citation
+   * @returns text
+   */
   private generateISO690(): string {
     // TODO: Implement ISO 690 citation generation
-    return 'ISO 690';
+    /*
+    *MODELO DE ESTRUCTURA
+APELLIDO/S DEL AUTOR/A, Nombre del autor/a. Título del artículo electrónico. En: Título de la revista en cursiva [en línea]. Año de publicación. Vol., n.º, págs. número de página inicial─número de página final. ISSN [consulta: ]. Disponible en: URL
+
+EJEMPLO
+​GÓMEZ I BLANCH, Guillem. Superfícies selectives per a la transformació tèrmica de l'energia solar. En: Revista de física [en línea]. 2010. Vol. 4, n.º 6, págs. 10─24. ISSN 2013-9845 [consulta: 29 de noviembre de 2019]. Disponible en: https://www.raco.cat/index.php/RevistaFisica/article/view/174205
+    */
+    const authors = this.item.allMetadataValues('dc.contributor.author');
+    const title = this.item.firstMetadataValue('dc.title');
+    //dc.relation.ispartof	Collectanea Botanica, 1946, vol. 1, num. 8, p. 95-105
+    const relationIsPartOf = this.item.firstMetadataValue('dc.relation.ispartof');
+    const date = this.item.firstMetadataValue('dc.date.issued');
+    const publisher = this.item.firstMetadataValue('dc.publisher');
+    const doi = this.item.firstMetadataValue('dc.identifier.doi');
+    const handle = this.item.firstMetadataValue('dc.identifier.uri');
+
+    let citation = '';
+
+    // today's date
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const day = today.getDate();
+    // [consulta: 29 de noviembre de 2019]
+    // Use toLocaleString to get the month name in the user's language
+    const monthName = today.toLocaleString(this.locale, { month: 'long' });
+    let formattedDate = '';
+    if (this.locale == 'ca_ES' || this.locale == 'ca') {
+      formattedDate = `[consulta: ${day} de ${monthName} de ${year}]`;
+    } else if (this.locale == 'es_ES' || this.locale == 'es') {
+      formattedDate = `[consulta: ${day} de ${monthName} de ${year}]`;
+    } else {
+      formattedDate = `[consulted: ${day} of ${monthName} of ${year}]`;
+    }
+
+    if (authors.length > 0) {
+      citation += this.formatAuthorsISO690(authors) + '. ';
+    }
+
+    // title
+    if (title) {
+      citation += `${title}. `;
+    }
+
+    // Cursive _journal title_
+    // substitute the underscores by <i> in html
+    if (relationIsPartOf) {
+      const parts = relationIsPartOf.split(',');
+      const journalTitle = parts[0];
+      const year = parts[1];
+      let volume = parts[2];
+      // if . is in the volume, remove it
+      if (volume.includes('.')) {
+        volume = volume.split('.')[1];
+        // remove the first space
+        volume = volume.trim();
+      }
+      let number = parts[3];
+      // if . is in the number, remove it
+      if (number.includes('.')) {
+        number = number.split('.')[1];
+        // remove the first space
+        number = number.trim();
+      }
+      let pages = parts[4];
+      // if . is in the pages, remove it
+      if (pages.includes('.')) {
+        pages = pages.split('.')[1];
+        // remove the first space
+        pages = pages.trim();
+      }
+      if (this.locale == 'ca_ES' || this.locale == 'ca') {
+        citation += `_${journalTitle}_. ${year}. Vol. ${volume}, núm. ${number}, pàgs. ${pages}. `;
+      } else if (this.locale == 'es_ES' || this.locale == 'es') {
+        citation += `_${journalTitle}_. ${year}. Vol. ${volume}, nº. ${number}, págs. ${pages}. `;
+      } else {
+        citation += `_${journalTitle}_. ${year}. Vol. ${volume}, num. ${number}, pags. ${pages}. `;
+      }
+    }
+
+
+    // consulted
+    if (formattedDate) {
+      citation += `${formattedDate}. `;
+    }
+
+    // issn
+    const issn = this.item.firstMetadataValue('dc.identifier.issn');
+    if (issn) {
+      citation += `ISSN: ${issn}. `;
+    }
+
+    // handle
+    if (handle) {
+      if (this.locale == 'ca_ES' || this.locale == 'ca') {
+        citation += `[Disponible a: ${handle}]`;
+      } else if (this.locale == 'es_ES' || this.locale == 'es') {
+        citation += `[Disponible en: ${handle}]`;
+      } else {
+        citation += `[Available at: ${handle}]`;
+      }
+    }
+
+    return citation;
   }
 
+  /**
+   * iso690 text to html  
+   * @param text
+   * @returns html
+   */
+  private textToHtml(text: string): string {
+    return text.replace(/_/g, '<i>');
+  }
+
+  /**
+   * Generate the APA citation
+   * @returns text
+   */
   private generateAPA(): string {
     const authors = this.item.allMetadataValues('dc.contributor.author');
     const title = this.item.firstMetadataValue('dc.title');
@@ -153,6 +301,10 @@ export class CitationExportComponent implements OnInit {
     return citation;
   }
 
+  /**
+   * Generate the MLA citation
+   * @returns text
+   */
   private generateMLA(): string {
     const authors = this.item.allMetadataValues('dc.contributor.author');
     const title = this.item.firstMetadataValue('dc.title');
@@ -185,6 +337,9 @@ export class CitationExportComponent implements OnInit {
     return citation;
   }
 
+  /**
+   * Generate the Chicago citation
+   */
   private generateChicago(): string {
     const authors = this.item.allMetadataValues('dc.contributor.author');
     const title = this.item.firstMetadataValue('dc.title');
@@ -222,6 +377,9 @@ export class CitationExportComponent implements OnInit {
     return this.generateChicago();
   }
 
+  /** 
+   * Generate the IEEE citation
+   */
   private generateIEEE(): string {
     const authors = this.item.allMetadataValues('dc.contributor.author');
     const title = this.item.firstMetadataValue('dc.title');
@@ -257,6 +415,9 @@ export class CitationExportComponent implements OnInit {
     return citation;
   }
 
+  /**
+   * Generate the BibTeX citation
+   */
   private generateBibTeX(): string {
     const authors = this.item.allMetadataValues('dc.contributor.author');
     const title = this.item.firstMetadataValue('dc.title');
@@ -299,6 +460,9 @@ export class CitationExportComponent implements OnInit {
     return citation;
   }
 
+  /**
+   * Generate the RIS citation
+   */
   private generateRIS(): string {
     const authors = this.item.allMetadataValues('dc.contributor.author');
     const title = this.item.firstMetadataValue('dc.title');
@@ -338,6 +502,26 @@ export class CitationExportComponent implements OnInit {
     return citation;
   }
 
+  /**
+   * Format the authors for ISO 690 citation
+   */
+  private formatAuthorsISO690(authors: string[]): string {
+    let formattedAuthors = '';
+      for (const author of authors) {
+        const parts = author.split(',');
+        if (parts.length >= 2) {
+          const lastName = parts[0].trim();
+          const firstName = parts[1].trim();
+          formattedAuthors += `${lastName.toUpperCase()}, ${firstName[0].toUpperCase() + firstName.slice(1).toLowerCase()}, `;
+        }
+    }
+    // clean the last comma and space
+    return formattedAuthors.slice(0, -2);
+  }
+
+  /**
+   * Format the authors for APA citation
+   */
   private formatAuthorsAPA(authors: string[]): string {
     if (authors.length === 1) {
       return this.formatSingleAuthorAPA(authors[0]);
@@ -350,6 +534,9 @@ export class CitationExportComponent implements OnInit {
     return '';
   }
 
+  /**
+   * Format the authors for MLA citation
+   */
   private formatAuthorsMLA(authors: string[]): string {
     if (authors.length === 1) {
       return this.formatSingleAuthorMLA(authors[0]);
@@ -361,6 +548,9 @@ export class CitationExportComponent implements OnInit {
     return '';
   }
 
+  /**
+   * Format the authors for Chicago citation
+   */
   private formatAuthorsChicago(authors: string[]): string {
     if (authors.length === 1) {
       return this.formatSingleAuthorChicago(authors[0]);
@@ -372,6 +562,9 @@ export class CitationExportComponent implements OnInit {
     return '';
   }
 
+  /**
+   * Format the authors for IEEE citation
+   */
   private formatAuthorsIEEE(authors: string[]): string {
     if (authors.length === 1) {
       return this.formatSingleAuthorIEEE(authors[0]);
@@ -383,6 +576,9 @@ export class CitationExportComponent implements OnInit {
     }
   }
 
+  /**
+   * Format single author for APA citation
+   */
   private formatSingleAuthorAPA(author: string): string {
     const parts = author.split(',');
     if (parts.length >= 2) {
@@ -393,6 +589,9 @@ export class CitationExportComponent implements OnInit {
     return author;
   }
 
+  /**
+   * Format single author for MLA citation
+   */
   private formatSingleAuthorMLA(author: string): string {
     const parts = author.split(',');
     if (parts.length >= 2) {
@@ -403,6 +602,9 @@ export class CitationExportComponent implements OnInit {
     return author;
   }
 
+  /**
+   * Format single author for Chicago citation
+   */
   private formatSingleAuthorChicago(author: string): string {
     const parts = author.split(',');
     if (parts.length >= 2) {
@@ -413,6 +615,9 @@ export class CitationExportComponent implements OnInit {
     return author;
   }
 
+  /**
+   * Format the authors for IEEE citation
+   */
   private formatSingleAuthorIEEE(author: string): string {
     const parts = author.split(',');
     if (parts.length >= 2) {
@@ -423,6 +628,11 @@ export class CitationExportComponent implements OnInit {
     return author;
   }
 
+  /**
+   * Get the filename for the citation
+   * @param format
+   * @returns filename
+   */
   private getFilename(format: string): string {
     const title = this.item.firstMetadataValue('dc.title') || 'citation';
     const sanitizedTitle = title.replace(/[^a-zA-Z0-9]/g, '_').substring(0, 50);
@@ -440,6 +650,11 @@ export class CitationExportComponent implements OnInit {
     return `${sanitizedTitle}_${format}.${extensions[format] || 'txt'}`;
   }
 
+  /*
+  * Download the citation to the user's computer
+  * @param content
+  * @param filename
+  */
   private downloadFile(content: string, filename: string): void {
     const blob = new Blob([content], { type: 'text/plain;charset=utf-8' });
     const url = window.URL.createObjectURL(blob);
